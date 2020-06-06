2bd49ca5a625d0ca7357bc429eee8f05e8a21b10
chriz-zeller <zeller.christina@gmx.net>
2020-06-06 16:41:33 +0200

    Fixes keypress/keydown behaviour. Cleans code.

--- js/gallery.js
+++ js/gallery.js
@@ -18,17 +18,20 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
   var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
 }
 
-document.body.addEventListener('keypress', function(e) {
+document.addEventListener('keydown', function(e) {
     if(e.key == "Escape"){
       closeModal ();
     }
